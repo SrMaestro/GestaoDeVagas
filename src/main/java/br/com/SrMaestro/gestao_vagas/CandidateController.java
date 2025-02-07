413218040df453d91ce8d19ce92ea3fbbe29f84b
Luis Felipe R . L <luisramires29032002@gmail.com>
2025-02-07 16:53:00 -0300

    chore: adiciona estrutura inicial do projeto Spring Boot

---
+++ src/main/java/br/com/SrMaestro/gestao_vagas/CandidateController.java
@@ -0,0 +1,22 @@
+package br.com.SrMaestro.gestao_vagas;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+    @PostMapping("/")
+    public void create(@RequestBody CandidateEndity candidateEndity){
+        System.out.println("Candidate");
+        System.out.println(candidateEndity.getName());
+
+
+
+
+    }
+}
--- src/main/java/br/com/SrMaestro/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/br/com/SrMaestro/gestao_vagas/GestaoVagasApplication.java
@@ -10,4 +10,6 @@ public class GestaoVagasApplication {
 		SpringApplication.run(GestaoVagasApplication.class, args);
 	}
 
+
+
 }
