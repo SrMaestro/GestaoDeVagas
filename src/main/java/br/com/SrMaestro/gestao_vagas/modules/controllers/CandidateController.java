f1fd31621bf066bbacf2d5d8d29fe7eb4c254ad1
Luis Felipe R . L <luisramires29032002@gmail.com>
2025-02-10 10:34:30 -0300

    fix: formatando menssagem de validacao

--- src/main/java/br/com/SrMaestro/gestao_vagas/modules/controllers/CandidateController.java
+++ src/main/java/br/com/SrMaestro/gestao_vagas/modules/controllers/CandidateController.java
@@ -1,6 +1,8 @@
-package br.com.SrMaestro.gestao_vagas;
+package br.com.SrMaestro.gestao_vagas.modules.controllers;
 
 
+import br.com.SrMaestro.gestao_vagas.modules.Enditys.CandidateEndity;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CandidateController {
 
     @PostMapping("/")
-    public void create(@RequestBody CandidateEndity candidateEndity){
+    public void create(@Valid @RequestBody CandidateEndity candidateEndity){
         System.out.println("Candidate");
         System.out.println(candidateEndity.getName());
 
